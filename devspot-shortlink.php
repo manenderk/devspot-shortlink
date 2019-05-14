63d04242d85c4daff29b769ccdf520b0535873b3
Manender Kumar <manender.kumar@mamsys.com>
2019-05-14 15:50:47 +0530

    shortlink updates

--- devspot-shortlink.php
+++ devspot-shortlink.php
@@ -17,6 +17,25 @@ define('PLUGIN_VER', '0.0.2');
 
 global $my_nonce;
 
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
 function createTables(){
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
@@ -130,7 +149,7 @@ function devspot_add_shortlink($args) {
     else{
         $shortLink->__set('redirectLink', $data['redirectLink']);
         global $wpdb;
-        $table_name = $wpdb->prefix . "devspot_shortlinks";
+        $table_name = $wpdb->prefix . SHORTLINK_TABLE;
         $wpdb->insert( 
             $table_name, 
             array( 
@@ -212,47 +231,68 @@ function devspot_shortlink_redirect(){
 
             $response = wp_remote_get("http://api.ipstack.com/$ip?access_key=e779deaa5f080abaf4d5feb40da9800f&format=1");
             $response = json_decode($response['body']);
-            var_dump($response['country_code']);
-
-            /*$ch = curl_init(); 
-            curl_setopt($ch,CURLOPT_URL,"http://api.ipstack.com/$ip?access_key=e779deaa5f080abaf4d5feb40da9800f&format=1");
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-            $output=curl_exec($ch);
-            curl_close($ch);
-            $output = json_decode($output);
-            echo $output['country_name'];
-            /*
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "http://api.ipstack.com/$ip?access_key=e779deaa5f080abaf4d5feb40da9800f&format=1",                    
-                CURLOPT_CUSTOMREQUEST => "GET",
-            ));
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $response = json_decode($response);*/
-            exit;
+            $response = (array)$response;
+            $country = $response['country_name'];
+            if($country === NULL)
+                $country = '';
+            
+            $wpdb->insert( 
+                $shortlinkStatsTable, 
+                array( 
+                    'shortLinkId' => $shortlink->id,
+                    'referer' => $referer, 
+                    'country' => $country, 
+                    'visited' => current_time('mysql', 1) 
+                ),array(
+                    '%d',
+                    '%s',
+                    '%s',
+                    '%s'
+                ) 
+            );
             wp_redirect($shortlink->redirectLink);
         }
     }
 }
 add_action('wp', 'devspot_shortlink_redirect');
 
+function devspot_get_stats($args) {
+    $response = [
+        'message' => '',
+        'status' => 'error'
+    ];
 
-function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
+    $current_user = wp_get_current_user();
+    $userId = $current_user->ID;
+
+    if(empty($userId)){
+        $response['message'] = 'invalid user';
+        $response['status'] = 'error';
+    }
+    else{
+        global $wpdb;
+        $shortlinkTable = $wpdb->prefix . SHORTLINK_TABLE;
+        $shortlinkStatsTable = $wpdb->prefix . SHORTLINK_STATS_TABLE;
+        $shortlinkStatsByName = $wpdb->get_results("SELECT count($shortlinkStatsTable.shortLinkId) AS clicks, $shortlinkTable.shortLink from $shortlinkStatsTable, $shortlinkTable WHERE $shortlinkStatsTable.shortLinkId = $shortlinkTable.id AND $shortlinkTable.userId = $userId GROUP BY $shortlinkStatsTable.shortLinkId", ARRAY_A);
+        $shortlinkTotalClicks = $wpdb->get_results("SELECT COUNT($shortlinkStatsTable.shortLinkId) as clicks FROM $shortlinkStatsTable, $shortlinkTable WHERE $shortlinkStatsTable.shortLinkId = $shortlinkTable.id AND $shortlinkTable.userId = $userId", ARRAY_A);
+        $shortlinkStatsByCountry = $wpdb->get_results("SELECT count($shortlinkStatsTable.shortLinkId) as clicks, $shortlinkStatsTable.country from $shortlinkStatsTable, $shortlinkTable WHERE $shortlinkStatsTable.shortLinkId = $shortlinkTable.id AND $shortlinkTable.userId = $userId GROUP BY $shortlinkStatsTable.country", ARRAY_A);
+        $shortlinkStatsByReferrer = $wpdb->get_results("SELECT count($shortlinkStatsTable.shortLinkId) as clicks, $shortlinkStatsTable.referer from $shortlinkStatsTable, $shortlinkTable WHERE $shortlinkStatsTable.shortLinkId = $shortlinkTable.id AND $shortlinkTable.userId = $userId GROUP BY $shortlinkStatsTable.referer", ARRAY_A);
+        $response['message'] = [
+            'totalClicks'   => $shortlinkTotalClicks,
+            'byClicks'      => $shortlinkStatsByName,
+            'byCountry'     => $shortlinkStatsByCountry,
+            'byReferer'     => $shortlinkStatsByReferrer
+        ];
+        $response['status'] = 'success';
+    }
+
+    $response = new WP_REST_Response( $response );
+    return $response; 
 }
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'dshortlink/v1', '/get-stats', array(
+		'methods' => 'GET',
+		'callback' => 'devspot_get_stats',
+	));
+});
+
